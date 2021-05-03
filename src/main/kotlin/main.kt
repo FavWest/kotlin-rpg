d4f2a737c313f729dfd9f5cf7df6e22c55c468b0
Tori FavWest <practiceandthenpractice@gmail.com>
2021-05-03 14:17:52 -0700

    add combat function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,7 +12,7 @@ fun main() {
         choice=readLine().toString()
         if(choice=="y"){
             println("With no real weapons available, you pick up a rock and hope for the best.")
-            val rock = Weapon("a rock", 2)
+            val rock = Weapon("rock", 2)
             mainChar.weapon = rock
         } else if(choice=="n"){
             println("You decide not to pick up the rock.")
@@ -32,14 +32,47 @@ fun main() {
     }
     choice=""
     val skitterer = Skitterer(0)
+    combat(mainChar, skitterer)
 }
 
 fun printInfo(mainChar: Creature){
     println("You are a level ${mainChar.level} ${mainChar.type}.")
     println("You have ${mainChar.armor.description} armor.")
-    println("You are armed with ${mainChar.weapon.description}.")
+    println("You are armed with your ${mainChar.weapon.description}.")
     println("Your attack is ${mainChar.attack}.")
     println("Your defense is ${mainChar.defense}.")
+}
+
+fun combat(mainChar: Creature, opponent: Creature){
+    var choice = ""
+    while(mainChar.hp>0 && opponent.hp>0) {
+        if (opponent.attack > mainChar.defense) {
+            mainChar.hp -= opponent.attack
+            println("")
+            println("The ${opponent.type} attacks with ${opponent.weapon.description}")
+            println("You take ${opponent.attack} damage and have ${mainChar.hp} hit points left")
+            println("")
+        } else {
+            println("You successfully defended yourself again the ${opponent.type}'s attack!")
+        }
+        println("What would you like to do? (type 'attack' to attack)")
+        choice = readLine().toString()
+        if (choice == "attack")
+            if (mainChar.attack > opponent.defense) {
+                opponent.hp -= mainChar.attack
+                println("")
+                println("You attack with ${mainChar.weapon.description}")
+                println("You deal ${mainChar.attack} damage")
+                println("")
+            } else {
+                println("Your attack was ineffective")
+            }
+    }
+    if(mainChar.hp==0){
+        println("You died")
+    } else {
+        println("You defeated the ${opponent.type}!")
+    }
 }
 
 open class Creature(var level: Int){
@@ -47,7 +80,8 @@ open class Creature(var level: Int){
     open var baseAttack = 0
     open var baseDefense = 0
     open var armor = Armor("no", 0)
-    open var weapon = Weapon("none", 0)
+    open var weapon = Weapon("unarmed attack", 0)
+    open var hp = 10
     var attack= 1
         get() = baseAttack + weapon.attack + level
     var defense = 0
@@ -58,13 +92,14 @@ class Goblin(level: Int) : Creature(level){
     override val type = "goblin"
     override var baseAttack = 1
     override var armor = Armor("no", 0)
-    override var weapon = Weapon("your fists", 0)
+    override var weapon = Weapon("fists", 0) //TODO generalize
 }
 
 class Skitterer(level: Int) : Creature(level){
     override val type = "skitterer"
     override var armor = Armor("chitinous exoskeleton", 2)
     override var weapon = Weapon("bite", 1)
+    override var hp = 1
 }
 
 class Weapon(val description: String, val attack: Int){
